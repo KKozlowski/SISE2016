e6ded4f4da96c79d9df95bf31cbbb3de5f7aac9f
kkozlowski <septuater@gmail.com>
2016-06-06 21:04:53 +0200

    REVERT to the previous version. It was misunderstanding.

--- Two/Two/main.cpp
+++ Two/Two/main.cpp
@@ -11,9 +11,9 @@
 using namespace std;
 
 element_list element_list::main;
-int length;
+
 void readFromStream() {
-	
+	int length;
 	cin >> length;
 
 	while (!cin.eof()) {
@@ -31,17 +31,17 @@ void main() {
 	clock_t tStart = clock();
 	/* Do your stuff here */
 
-	classifier::qualify_sets(pair.second, pair.first, MetricType::EuklideanMetric, length, ChosingType::FirstOnly, false);
-	classifier::qualify_sets(pair.second, pair.first, MetricType::ManhattanMetric, length, ChosingType::FirstOnly, false);
-	classifier::qualify_sets(pair.second, pair.first, MetricType::CzebyszewMetric, length, ChosingType::FirstOnly, false);
-	cout << endl << endl;														 
-	classifier::qualify_sets(pair.second, pair.first, MetricType::EuklideanMetric, length, WeightedSum, false);
-	classifier::qualify_sets(pair.second, pair.first, MetricType::ManhattanMetric, length, WeightedSum, false);
-	classifier::qualify_sets(pair.second, pair.first, MetricType::CzebyszewMetric, length, WeightedSum, false);
-	cout << endl << endl;														  
-	classifier::qualify_sets(pair.second, pair.first, MetricType::EuklideanMetric, length, ChosingType::MostAppearances, false);
-	classifier::qualify_sets(pair.second, pair.first, MetricType::ManhattanMetric, length, ChosingType::MostAppearances, false);
-	classifier::qualify_sets(pair.second, pair.first, MetricType::CzebyszewMetric, length, ChosingType::MostAppearances, false);
+	classifier::qualify_sets(pair.second, pair.first, MetricType::EuklideanMetric, 5, ChosingType::FirstOnly, false);
+	classifier::qualify_sets(pair.second, pair.first, MetricType::ManhattanMetric, 5, ChosingType::FirstOnly, false);
+	classifier::qualify_sets(pair.second, pair.first, MetricType::CzebyszewMetric, 5, ChosingType::FirstOnly, false);
+	cout << endl << endl;
+	classifier::qualify_sets(pair.second, pair.first, MetricType::EuklideanMetric, 5, WeightedSum, false);
+	classifier::qualify_sets(pair.second, pair.first, MetricType::ManhattanMetric, 5, WeightedSum, false);
+	classifier::qualify_sets(pair.second, pair.first, MetricType::CzebyszewMetric, 5, WeightedSum, false);
+	cout << endl << endl;
+	classifier::qualify_sets(pair.second, pair.first, MetricType::EuklideanMetric, 5, ChosingType::MostAppearances, false);
+	classifier::qualify_sets(pair.second, pair.first, MetricType::ManhattanMetric, 5, ChosingType::MostAppearances, false);
+	classifier::qualify_sets(pair.second, pair.first, MetricType::CzebyszewMetric, 5, ChosingType::MostAppearances, false);
 
 	printf("TIME: %.5fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
 
